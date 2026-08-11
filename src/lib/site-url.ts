a0ef3269bcb2b5a6629b40695085766744baf164
Samvrit Srinath <sasrinath@ucsd.edu>
2026-08-11 08:56:12 -0700

    Fixing site URL.

--- src/lib/site-url.ts
+++ src/lib/site-url.ts
@@ -1,4 +1,4 @@
 const rawSiteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "https://stavyas.vercel.app";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://stavyasrinath.vercel.app";
 
 export const siteUrl = rawSiteUrl.startsWith("http") ? rawSiteUrl : `https://${rawSiteUrl}`;
